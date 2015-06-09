5bcc526967454b2600f16cbe5740452fdb5c9868
brecht <brecht.baeten@gmail.com>
2015-06-09 10:56:34 +0200

    added method to load results

--- __init__.py
+++ __init__.py
@@ -12,6 +12,9 @@
 
 import win32ui
 import dde
+import os
+import scipy.io
+import string
 
 class Dymola:
 	def __init__(self):
@@ -62,4 +65,37 @@ class Dymola:
 				self.run_cmd( '%s=%s'%(key,val) )
 			except:
 				print( 'could not assign %s to %s'%(key,val) )
+				
+	def get_result(self):
+		"""
+		loads results into a dictionary
+		"""
+
+		fileName = 'dsres.mat'
+		fullFileName = os.path.abspath(fileName)
+
+		fileData = scipy.io.loadmat(fullFileName, matlab_compatible=True)
+
+		name = fileData["name"].T
+		description = fileData["description"].T
+		dataInfo = fileData["dataInfo"].T
+		data_1 = fileData["data_1"]
+		data_2 = fileData["data_2"]
+
+		namelist = []
+		for item in name:
+			namelist.append(str(string.rstrip(string.join([x for x in item if len(x) > 0 and ord(x) < 128], ""))))
+
+		name = namelist
+		
+		res = {}
+		for idx,item in enumerate(name):
+			if dataInfo[idx,0] == 1:
+				# create a linspace  vector with the same length as data
+				res[item] = scipy.sign(dataInfo[idx,1]) * scipy.linspace(data_1[abs(dataInfo[idx,1])-1][0], data_1[abs(dataInfo[idx,1])-1][1], len(data_2[0]) ) 
+			else:
+				res[item] = scipy.sign(dataInfo[idx,1]) * data_2[abs(dataInfo[idx,1])-1]
+		
+		return res
+		
 		
